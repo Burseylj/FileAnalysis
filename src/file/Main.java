bff03a0afeb35051c7919b0b6eb6706862d412b7
burseylj <burseylj@users.noreply.github.com>
2018-10-25 19:37:29 -0400

    Fixed file package's main function

--- src/file/Main.java
+++ src/file/Main.java
@@ -5,8 +5,8 @@ import java.io.File;
 public class Main {
 
 	public static void main(String[] args) {
-		File file = new File("C:/Users/anas/Documents/JavaLearning/EclipseFiles/Ozymandias.txt");
-		File file2 = new File("C:/Users/anas/Documents/JavaLearning/EclipseFiles/test/PizzaBoy.txt");
+		File file = new File("texts/GinsbergBeatHowl.txt");
+		File file2 = new File("texts/GlobeAndMail.txt");
 		Analysis analysis = new Analysis(file);
 		try {
 		    System.out.println("Lines: " + analysis.lines());
